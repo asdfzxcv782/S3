407d2e95527cde4e27bd5de9b1f59277cd73e6cc
Richy <w4567892015@gmail.com>
2016-10-03 11:12:56 +0800

    add auth

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var nunjucks = require('nunjucks');
 var express = require('express');
+var bodyParser = require('body-parser');
+var auth = require('basic-auth');
 var app = express();
 
 var http = require('http').Server(app);
@@ -25,6 +27,26 @@ nunjucks.configure(__dirname + '/views', { //模板引擎設定
 
 app.use(express.static(__dirname + '/public')); //靜態檔案處理
 
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// BasicAuth login
+app.use(function(req, res, next) {
+    var user = auth(req);
+    if (user === undefined || user['name'] !== 'ushow' || user['pass'] !== 'vshow2351700') {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm=Authorization Required');
+        res.json({
+            login:false
+        });
+        res.end();
+    } else {
+        next();
+    }
+});
+
 app.get('/', function (req, res){
   res.render('aws_s3_upload_file.html', {
     title : 'Aws-S3-Upload-File',
@@ -49,104 +71,3 @@ app.use(function(err, req, res, next) {
 });
 
 var s3Socket = new s3Socket(io,systemOption);
-
-//var online = 0;
-//
-//var filedata=[], fileCount = 0, MaxFile = 0 , file_path='';
-//
-//io.on('connection', function(socket){ //io 廣播用 socket 私密用
-//    
-//    console.log('a user connected *:9696');
-//    
-//    online++;
-//    
-//    io.emit('online', { online: online });
-//    
-//    var s3 = new s3_function(socket);
-//    
-//    s3.get_game_file_list('ushowgamefile');
-//        
-//    socket.on('upload_file_data',function(msg){
-//        
-//        fileCount = 0; //重置
-//        
-//        MaxFile = msg.filedata.length;
-//        filedata = msg.filedata;
-//        file_path = msg.file_path;
-//        
-//        socket.emit('even_file_upload', { fileCount });
-//        
-//    });
-//    
-//    ss(socket).on('upload_file', function(stream) {
-//        
-//        var file_stream =stream.pipe(fs.createWriteStream('./tmp/'+filedata[fileCount],{flags: 'a',encoding: 'utf8'}));
-//                
-//        file_stream.on('close',function(){
-//            
-//            var file_data_tmp_path = './tmp/'+filedata[fileCount];
-//            
-//            var body = fs.createReadStream(file_data_tmp_path);
-//            
-//            s3.upload_gmae_file('ushowgamefile',(file_path + filedata[fileCount]),body,function(even){
-//                if(even){
-//                    fs.unlink(file_data_tmp_path);
-//                    console.log('del tmp file : ' + file_data_tmp_path);
-//                }
-//                
-//                s3.get_game_file_list('ushowgamefile');
-//                
-//            }.bind(this));
-//            
-//            body.on('close',function(){
-//                
-//                fileCount++;
-//            
-//                if(fileCount < MaxFile){
-//                    console.log(filedata[(fileCount-1)]+' end!')
-//                    socket.emit('even_file_upload', { fileCount });
-//                }else{
-//                    console.log(filedata[(fileCount-1)]+' end!')
-//                    console.log('file stream end!')
-//                    socket.emit('even_file_end', { });
-//                }
-//                
-//            });
-//            
-//            
-//            
-//        });
-//        
-//    });
-//    
-//    socket.on('get_file_info',function(msg){
-//        s3.get_file_info('ushowgamefile',msg.get_file_info);
-//    });
-//    
-//    socket.on('add_folder',function(msg){
-//        s3.add_folder('ushowgamefile',msg.add_folder,function(data){
-//            if(data){
-//                s3.get_game_file_list('ushowgamefile');
-//            }
-//        });
-//    });
-//    
-//    socket.on('del_file',function(msg){
-//        s3.del_file('ushowgamefile',msg.del_file,function(data){
-//            if(data){
-//                s3.get_game_file_list('ushowgamefile');
-//            }
-//        }.bind(this));
-//    });
-//        
-//    socket.on('disconnect', function(){
-//        
-//        console.log('user disconnected *:3004');
-//        
-//        online--;
-//        
-//        io.emit('online', { online: online });
-//        
-//    });
-//    
-//});

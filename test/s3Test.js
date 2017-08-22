000a2d6a7e7b554e1bd154e709b86efc1cfacb3e
RichyYang <w4567892015@gmail.com>
2017-08-22 09:08:11 +0800

    add html

---
+++ test/s3Test.js
@@ -0,0 +1,33 @@
+"use strict"; //use es6
+
+var assert = require('assert');
+//var S3App = require('../app.js')('', 'game123456');
+
+describe('S3 Testing...', function() {
+    // it('Create S3', function (done) {
+    //     S3App instanceof Object;
+    //     done();
+    // });
+    // it('Get S3 list', function (done) {
+    //     this.timeout(6000);
+    //     S3App.getObjectList((status, data, errorCode)=>{
+    //         if(status){
+    //             done();
+    //         }else{
+    //             done(new Error(errorCode));
+    //         }
+    //     });
+    // });
+    // 
+    // it('Create folder', function (done) {
+    //     this.timeout(6000);
+    //     S3App.creatFolder('./test', 'testing', (status, data, errorCode)=>{
+    //         if(status){
+    //             console.log(data);
+    //             done();
+    //         }else{
+    //             done(new Error('data is Null'));
+    //         }
+    //     });
+    // });
+});
